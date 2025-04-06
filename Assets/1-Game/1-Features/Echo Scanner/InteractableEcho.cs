e9bbd9552d73164cfa603681e23f203442ad5116
TJGameDev <98005357+TJGameDev@users.noreply.github.com>
2025-04-06 14:57:21 -0700

    interactables ping yellow

---
+++ Assets/1-Game/1-Features/Echo Scanner/InteractableEcho.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using Game.Sounds;
+using UnityEngine;
+
+namespace LD57.Echo
+{
+public class InteractableEcho : InvisibleObject
+{
+    [SerializeField] private ParticleSystem echoParticleSystem;
+    [SerializeField] private AudioClip echoSound;
+    public override void EmitEcho()
+    {
+        base.EmitEcho();
+        echoParticleSystem.Play();
+        SoundManager.PlayWorld(echoSound, transform.position, 0.4f, 1f, false);
+    }
+}
+}
