4bde0466374b868ef2beba70aa047a8ad0687899
AlexRak2 <56671393+AlexRak2@users.noreply.github.com>
2025-04-05 21:16:43 -0400

    Centipede

---
+++ Assets/1-Game/1-Features/Centipede/CentipededAI.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Game.Centipede
+{
+    public class CentipededAI : MonoBehaviour
+    {
+        [Header("Wander Settings")]
+        public float wanderRadius = 10f;
+        public float wanderInterval = 5f;
+        public float movementSpeed = 1f;
+
+        private NavMeshAgent _agent;
+        private float _timer;
+
+        void Start()
+        {
+            _agent = GetComponent<NavMeshAgent>();
+            _agent.speed = movementSpeed;
+            _timer = wanderInterval;
+        }
+
+        void Update()
+        {
+            _timer += Time.deltaTime;
+
+            if (_timer >= wanderInterval || _agent.velocity.sqrMagnitude == 0f)
+            {
+                Vector3 newPos = GetRandomNavMeshPosition(transform.position, wanderRadius);
+                _agent.SetDestination(newPos);
+                _timer = 0;
+            }
+        }
+
+        private Vector3 GetRandomNavMeshPosition(Vector3 center, float radius)
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                Vector3 randomPos = center + Random.insideUnitSphere * radius;
+                if (NavMesh.SamplePosition(randomPos, out NavMeshHit hit, 2.0f, NavMesh.AllAreas))
+                {
+                    return hit.position;
+                }
+            }
+            return center;
+        }
+    }
+}
